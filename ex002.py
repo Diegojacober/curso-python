fb89b2af6f90730768010023e148bedd2e6fd649
Diego <diegoalencar.jacober@gmail.ocm>
2023-01-29 20:48:27 -0300

    29/01/2023

---
+++ ex002.py
@@ -0,0 +1,2 @@
+nome = input('Qual seu nome?')
+print('Seja bem vindo, {}'.format(nome))
---
+++ ex003.py
@@ -0,0 +1,4 @@
+valor1 = int(input('Digite um valor'))
+valor2 = int(input('Digite outro valor'))
+soma = (valor1 + valor2)
+print('A soma entre {} e {} é {}'.format(valor1,valor2,soma))
