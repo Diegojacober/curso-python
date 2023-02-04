3530db1e425a1c786b7047afa94e3c2f27618833
Diego <diegoalencar.jacober@gmail.ocm>
2023-02-04 09:20:11 -0300

    Exercicios utilizando módulos do python

---
+++ ex20.py
@@ -0,0 +1,9 @@
+import random
+
+aluno1 = input('Digite o nome do primeiro aluno: ')
+aluno2 = input('Digite o nome do segundo aluno: ')
+aluno3 = input('Digite o nome do terceiro aluno: ')
+aluno4 = input('Digite o nome do quarto aluno: ')
+lista = [aluno1, aluno2, aluno3, aluno4]
+random.shuffle(lista)
+print('A ordem de apresentação será: \n {}'.format(lista))
