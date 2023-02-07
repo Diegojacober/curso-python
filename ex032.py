be24d7487084941cf5f4242ef8cb7ce34d3d42d5
Desenvolvimento <dsadm@educ501.local>
2023-02-07 13:52:19 -0300

    Exercicio anos bissextos

---
+++ ex032.py
@@ -0,0 +1,10 @@
+from datetime import date
+
+ano = int(input('Qual ano você deseja analisar? Digite 0 para o ano atual '))
+
+if ano == 0:
+    ano = date.today().year
+if ano % 4 == 0 and ano % 100 != 0 or ano % 400 == 0:
+    print(f'O ano {ano} é bissexto')
+else:
+    print(f'O ano {ano} não é bissexto')
