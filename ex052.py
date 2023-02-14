b1b54c03a2bf1f8aa2af9eac21ac691d1820779e
Desenvolvimento de Sistemas <desenvolvimentods@educ501.local>
2023-02-14 13:38:04 -0300

    Exercicio verificando número primo

---
+++ ex052.py
@@ -0,0 +1,14 @@
+num = int(input('Digite um número: '))
+tot = 0
+for c in range(1, num + 1):
+    if num % c == 0:
+        print('\033[34m', end=' ')
+        tot += 1
+    else:
+        print('\033[31m', end=' ')
+    print('{}'.format(c), end=' ')
+print('\n\033[mO número {} foi divisível {} vezes '.format(num,tot))
+if tot == 2:
+    print('E por isso ele é primo')
+else:
+    print('E por isso ele não é primo')
