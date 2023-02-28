a841e8119425ef78705b001c7422dd5a34559a94
Diego Alencar Jacober <81656395+Diegojacober@users.noreply.github.com>
2023-02-28 13:37:08 +0000

    Atualizando exercicio 58 - apenas números primos

--- ex058.py
+++ ex058.py
@@ -1,11 +1,23 @@
 from random import randint
 numeroUser = int(input('tente adivinhar o numero inteiro: '))
-numeroAleatorio = randint(0, 10)
+numeroAleatorio = randint(0, 100)
 tentativas = 1
-while numeroUser != numeroAleatorio:
-    if numeroUser > numeroAleatorio:
-     numeroUser = int(input('Menos..., tente novamente: '))
-    if numeroUser < numeroAleatorio:
-        numeroUser = int(input('Mais..., tente novamente: '))
-    tentativas += 1
-print('Você precisou de {} tentativas'.format(tentativas))
+
+mult = 0
+
+while True:
+    for count in range(2, numeroAleatorio):
+        if numeroAleatorio % count == 0:
+            mult += 1
+            numeroAleatorio = randint(0, 100)
+        mult = 0
+        
+    if mult == 0:
+        while numeroUser != numeroAleatorio:
+            if numeroUser > numeroAleatorio:
+                numeroUser = int(input('Menos..., tente novamente: '))
+            if numeroUser < numeroAleatorio:
+                numeroUser = int(input('Mais..., tente novamente: '))
+            tentativas += 1
+        print(f'Você precisou de {tentativas} tentativas')
+        break
