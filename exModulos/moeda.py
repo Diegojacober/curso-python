f3030231215302b4e6f7ba6cc4237168d61a3ced
Diego Alencar Jacober <57761933898>
2023-03-09 16:56:36 -0300

    Exercicio 110 - funcao de resumo

--- exModulos/moeda.py
+++ exModulos/moeda.py
@@ -1,11 +1,11 @@
-def aumentar(valor,porcentagem,format=False,formatacao='R$'):
+def aumentar(valor,porcentagem=10,format=False,formatacao='R$'):
     aumento = valor * (porcentagem / 100)
     if format:
         return moeda(valor+aumento,formatacao)
     return valor + aumento
 
 
-def diminuir(valor,porcentagem,format=False,formatacao='R$'):
+def diminuir(valor,porcentagem=10,format=False,formatacao='R$'):
     desconto = valor * (porcentagem / 100)
     if format:
         return moeda(valor - desconto,formatacao)
@@ -22,6 +22,17 @@ def metade(valor,format=False,formatacao='R$'):
      if format:
         return moeda(valor/2,formatacao)
      return valor/2
+ 
+def resumo(valor,porcentagem=10,format=False,formatacao='R$'):
+    print('-'*30)
+    print('RESUMO DO VALOR'.center(30))
+    print(f'PREÇO ANALISADO: \t {moeda(valor)}')
+    print(f'DOBRO DO VALOR: \t {dobro(valor,format,formatacao)}')
+    print(f'A METADE DO VALOR É \t {metade(valor,format,formatacao)}')
+    print(f'O VALOR COM DESCONTO DE 10% É \t {diminuir(valor,porcentagem,format,formatacao)}')
+    print(f'O VALOR COM AUMENTO DE 10% É \t {aumentar(valor,porcentagem,format,formatacao)}')
+    print('-'*30)
+     
 
 def moeda(valor=0,moeda='R$'):
     return f'{moeda}{valor:.2f}'.replace('.',',')
