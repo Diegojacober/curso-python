f369f2a81f321ba0f14514cb3d3e0611891e7602
Diego Alencar Jacober <57761933898>
2023-03-09 16:08:15 -0300

    Exercicio 109 - Formatando atraves da propria funcao

--- exModulos/main.py
+++ exModulos/main.py
@@ -2,7 +2,7 @@ import moeda
 
 
 p = float(input('Digite o preço: R$'))
-print(f'A metade de {p} é {moeda.moeda(valor=moeda.metade(p),moeda="R$")}')
-print(f'O dobro de {p} é {moeda.moeda(valor=moeda.dobro(p),moeda="R$")}')
-print(f'Aumentando 10% de {p}, teremos {moeda.moeda(valor=moeda.aumentar(p, 10),moeda="R$")}')
-print(f'Diminuindo 10% de {p}, teremos {moeda.moeda(valor=moeda.diminuir(p, 10),moeda="$")}')
+print(f'A metade de {p} é {moeda.metade(p,format=True)}')
+print(f'O dobro de {p} é {moeda.dobro(p,format=True,formatacao="U$")}')
+print(f'Aumentando 10% de {p}, teremos {moeda.aumentar(p, 10,True,"R$")}')
+print(f'Diminuindo 10% de {p}, teremos {moeda.diminuir(valor=p, porcentagem=10,format=True,formatacao="$")}')
--- exModulos/moeda.py
+++ exModulos/moeda.py
@@ -1,19 +1,27 @@
-def aumentar(valor,porcentagem):
+def aumentar(valor,porcentagem,format=False,formatacao='R$'):
     aumento = valor * (porcentagem / 100)
+    if format:
+        return moeda(valor+aumento,formatacao)
     return valor + aumento
 
 
-def diminuir(valor,porcentagem):
+def diminuir(valor,porcentagem,format=False,formatacao='R$'):
     desconto = valor * (porcentagem / 100)
+    if format:
+        return moeda(valor - desconto,formatacao)
     return valor - desconto
 
 
-def dobro(valor):
+def dobro(valor,format=False,formatacao='R$'):
+    if format:
+        return moeda(valor * 2,formatacao)
     return valor * 2
 
 
-def metade(valor):
-    return valor/2
+def metade(valor,format=False,formatacao='R$'):
+     if format:
+        return moeda(valor/2,formatacao)
+     return valor/2
 
 def moeda(valor=0,moeda='R$'):
     return f'{moeda}{valor:.2f}'.replace('.',',')
