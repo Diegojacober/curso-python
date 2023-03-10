e4f818937ce4d484171f0448c8dc5f38aa699b60
Diego Alencar Jacober <57761933898>
2023-03-10 14:07:05 -0300

    Exercicio 112 - tratando os dados

--- exModulos/main.py
+++ exModulos/main.py
@@ -1,7 +1,8 @@
 from utilidades import moeda
+from utilidades import dado
 
 
-p = float(input('Digite o preço: R$'))
+p = dado.leiaDinheiro('Digite o preço: R$')
 print(f'A metade de {p} é {moeda.metade(p,format=True)}')
 print(f'O dobro de {p} é {moeda.dobro(p,format=True,formatacao="U$")}')
 print(f'Aumentando 10% de {p}, teremos {moeda.aumentar(p, 10,True,"R$")}')
