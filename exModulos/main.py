660b280767fa9fd3f30f29f33c54dba5cae14e78
Diego Alencar Jacober <57761933898>
2023-03-10 13:35:44 -0300

    Exercicio 111 - transformando em pacotes

--- exModulos/main.py
+++ exModulos/main.py
@@ -1,4 +1,4 @@
-import moeda
+from utilidades import moeda
 
 
 p = float(input('Digite o preço: R$'))
@@ -6,4 +6,4 @@ print(f'A metade de {p} é {moeda.metade(p,format=True)}')
 print(f'O dobro de {p} é {moeda.dobro(p,format=True,formatacao="U$")}')
 print(f'Aumentando 10% de {p}, teremos {moeda.aumentar(p, 10,True,"R$")}')
 print(f'Diminuindo 10% de {p}, teremos {moeda.diminuir(valor=p, porcentagem=10,format=True,formatacao="$")}')
-moeda.resumo(valor=p,porcentagem=10,format=True)
+moeda.resumo(valor=p, porcentagem=10, format=True)
