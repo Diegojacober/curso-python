799cccc7f399d120d8beb734d31b2bf30f0091a1
Diego <diegoalencar.jacober@gmail.ocm>
2023-03-11 08:47:40 -0300

    Exercicio 1

---
+++ Constantes_id_flgs/Exercicios/Enunciado.py
@@ -0,0 +1,14 @@
+"""
+1 - Faça um programa que peça ao usuário para digitar um número inteiro,
+informe se este número é par ou ímpar. Caso o usuário não digite um número inteiro,
+informe que não é um numero inteiro
+
+
+2 - Faça um programa que pergunte a hora ao usuário e, baseando-se no horário
+descrito, exiba a saudação apropriada. Ex:
+    Bom dia (00:00 - 11:59), Boa Tarde (12:00 - 17:59)  e Boa noite (18:00 - 23:59)
+    
+3 - Faça um programa que peça o primeiro nome do usuário. Se o nome tiver 4 letras ou
+menos, escreva "seu nome é curto". Se tiver entre 5 e 6 letras, escreva "Seu nome é médio";
+Maior que 6 letras, escreva "Seu nome é longo" 
+"""
---
+++ Constantes_id_flgs/Exercicios/exercicio1.py
@@ -0,0 +1,18 @@
+def digite_int(msg):
+    while True:
+        n = input(msg).strip()
+        try:
+            n = int(n)
+        except (ValueError,TypeError):
+            print('\033[31mTipo inválido, digite um numero inteiro\033[m')
+        else:
+            break
+    return n
+        
+n = digite_int('Digite um número: ')
+
+if n % 2 == 0:
+    print(f'{n} número é par')
+else:
+    print(f'{n} número é impar')
+    
