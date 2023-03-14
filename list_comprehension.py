7599024aacc8006b836dfec395ca9370027d4167
Diego Alencar Jacober <57761933898>
2023-03-14 13:46:30 -0300

    Criando listas com list comprehension

--- list_comprehension.py
+++ list_comprehension.py
@@ -12,3 +12,16 @@ print(lista)
 #com comprehension
 lista_1 = [(numero + 1) * 2 for numero in range(10)]
 print(lista_1)
+
+#mapear dados em list comprehension
+produtos = [
+    {'nome': 'p1', 'preco': 20,},
+    {'nome': 'p2', 'preco': 10},
+    {'nome': 'p3', 'preco': 30},
+]
+
+# novos_produtos = [{'nome': produto['nome'], 'preco_imposto': produto['preco'] * 2.5} for produto in produtos]
+
+novos_produtos = [{**produto, 'preco': (produto['preco'] * 1.10)} if produto['preco'] > 20 else {**produto} for produto in produtos]
+
+print(*novos_produtos, sep='\n')
