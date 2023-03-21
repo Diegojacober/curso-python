01f0413c156bafaacfc91efba7e5a5a70e5f0c74
Diego <diegoalencar.jacober@gmail.ocm>
2023-03-21 19:06:09 -0300

    Sobreposição de metodos e atributos

--- POO/aula14.py
+++ POO/aula14.py
@@ -17,23 +17,37 @@ super() é a superClasse na sub-classe
 
 class A:
     atributo_a = 'valor A'
+    
+    def __init__(self, valor) -> None:
+        self.valor = valor
+    
     def metodo(self):
         print('A')
         
 class B(A):
     atributo_b = 'valor B'
+    
+    def __init__(self, valor) -> None:
+        super().__init__(valor)
+    
     def metodo(self):
         print('B')
         
 class C(B):
     atributo_c = 'valor C'
+    
+    def __init__(self, *args, **kwargs) -> None:
+        
+        super().__init__(*args, **kwargs)
+    
     def metodo(self):
         # super(B,self).metodo()
         super().metodo()
         print('C')
         
-C = C()
+C = C('valor')
 print(C.atributo_a)
 print(C.atributo_b)
 print(C.atributo_c)
 C.metodo()
+print(C.valor)
