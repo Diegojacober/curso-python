96670a0682330c710f08358fd55b9d9f34f16f94
Diego <diegoalencar.jacober@gmail.ocm>
2023-04-15 12:05:51 -0300

    Exercicio com função closure

--- funcoes_closure.py
+++ funcoes_closure.py
@@ -2,18 +2,37 @@
 Closure e funções que retornam outras funções
 """
 
-def criar_saudacao(saudacao):
-    def saudar(nome):
-        return f'{saudacao}, {nome}!'
-    # tirar parenteses retorna a função e não a executa
-    return saudar
+# def criar_saudacao(saudacao):
+#     def saudar(nome):
+#         return f'{saudacao}, {nome}!'
+#     # tirar parenteses retorna a função e não a executa
+#     return saudar
 
-dizer_bom_dia = criar_saudacao('Bom dia')
-dizer_boa_noite = criar_saudacao('Boa noite')
+# dizer_bom_dia = criar_saudacao('Bom dia')
+# dizer_boa_noite = criar_saudacao('Boa noite')
 
-# adiando a execução
-print(dizer_bom_dia('Gabriel'))
-print(dizer_boa_noite('João'))
+# # adiando a execução
+# print(dizer_bom_dia('Gabriel'))
+# print(dizer_boa_noite('João'))
 
-for nome in ['Diego', 'Cadu', 'Angelo', 'Nicole']:
-    print(dizer_bom_dia(nome))
+# for nome in ['Diego', 'Cadu', 'Angelo', 'Nicole']:
+#     print(dizer_bom_dia(nome))
+    
+    
+#exercicio
+#crie funções que duplicam, triplicam e quadruplicam o numero recebido como parametro
+
+def multiplicador(multiplicador):
+    def multiplicarNumero(num):
+        return multiplicador * num
+    return multiplicarNumero
+
+duplicar = multiplicador(2)
+print(duplicar(5))
+
+
+triplicar = multiplicador(3)
+print(triplicar(5))
+
+quadruplicar = multiplicador(4)
+print(quadruplicar(5))
