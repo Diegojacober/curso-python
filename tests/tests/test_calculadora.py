70a8f468d3d1e8e204a3c4293481c4de637fdc58
Diego Alencar Jacober <57761933898>
2023-09-28 14:47:46 -0300

    Organizando e executando todos os testes

---
+++ tests/tests/test_calculadora.py
@@ -0,0 +1,50 @@
+try: 
+    import os
+    import sys
+    
+    sys.path.append(
+        os.path.abspath(
+            os.path.join(
+                os.path.dirname(__file__), '../src'
+            )
+        )
+    )
+    
+except: 
+    raise
+
+import unittest
+from calculadora import soma
+
+class TestCalculadora(unittest.TestCase):
+    def test_soma_5_e_5_deve_retornar_10(self):
+        self.assertEqual(soma(5, 5), 10)
+        
+    def teste_soma_5_negativo_e_5_deve_retornar_0(self):
+        self.assertEqual(soma(-5, 5), 0)
+        
+    def test_soma_varias_entradas(self):
+        x_y_saidas = (
+            (10, 10, 20),
+            (5, 5, 10),
+            (1.5, 1.5, 3.0),
+            (-5, 5, 0),
+            (100, 100, 200)
+        )
+        
+        for x_y_saida in x_y_saidas:
+            with self.subTest(x_y_saida=x_y_saida):
+                x, y, saida = x_y_saida
+                self.assertEqual(soma(x, y), saida)
+                
+    
+    def test_soma_param_x_nao_e_int_ou_float_deve_retornar_assertionerror(self):
+        with self.assertRaises(AssertionError):
+            soma('41', 10)
+            
+    def test_soma_param_y_nao_e_int_ou_float_deve_retornar_assertionerror(self):
+        with self.assertRaises(AssertionError):
+            soma(25, '56')
+    
+if __name__ == "__main__":
+    unittest.main(verbosity=2)
